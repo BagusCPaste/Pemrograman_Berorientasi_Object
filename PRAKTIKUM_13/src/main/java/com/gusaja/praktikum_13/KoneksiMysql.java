8a9fc6029bd52e54259bf6d4aeda07c8e88247ef
BagusCPaste <78890049+BagusCPaste@users.noreply.github.com>
2023-07-17 22:52:15 +0700

    Java Netbeans

---
+++ PRAKTIKUM_13/src/main/java/com/gusaja/praktikum_13/KoneksiMysql.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.gusaja.praktikum_13;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author muhba
+ */
+class KoneksiMysql {
+
+    KoneksiMysql(String localhost, String root, String string, String penjualan) {
+    }
+
+    Connection getConnection() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ PRAKTIKUM_13/src/main/java/com/gusaja/praktikum_13/PRAKTIKUM_13.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.gusaja.praktikum_13;
+
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JRException;
+
+/**
+ *
+ * @author muhba
+ */
+public class PRAKTIKUM_13 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
